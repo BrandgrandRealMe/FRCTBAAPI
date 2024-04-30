9b1c4a9f4aa951493773b343269858cd7f79e14c
BrandgrandRealMe <brandon@brandgrand.tech>
2024-04-30 09:15:47 -0500

    Added TS file and done some Fixes: removed console logs and added token instead of process.env.TBATOKEN

--- src/typescript.ts
+++ src/typescript.ts
@@ -1,32 +1,33 @@
-const baseUrl = "https://www.thebluealliance.com/api/v3";
-let token = null;
+const baseUrl: string = "https://www.thebluealliance.com/api/v3";
+let token: string | null = null;
 
-export function TBAaddToken(newToken) {
+export function TBAaddToken(newToken: string): void {
   // Store the token in a global variable
   token = newToken;
 }
 
-export async function teamInfo(team, year, options = {}) {
-  const url = `${baseUrl}/team/frc${team}`;
+export async function teamInfo(team: string, year: number, options: any = {}): Promise<any> {
+  const url: string = `${baseUrl}/team/frc${team}`;
   options.headers = {
     "X-TBA-Auth-Key": token,
+  };
 
   return await fetch(url, options)
     .then((response) => response.json())
     .then((data) => data);
 }
 
-export async function teamLogo(team, options = {}) {
-  const date = new Date();
-  const year = date.getFullYear();
-  const url = `${baseUrl}/team/frc${team}/media/${year}`;
+export async function teamLogo(team: string, options: any = {}): Promise<any> {
+  const date: Date = new Date();
+  const currentYear: number = date.getFullYear();
+  const url: string = `${baseUrl}/team/frc${team}/media/${currentYear}`;
   options.headers = {
     "X-TBA-Auth-Key": token,
   };
 
   const mediadata = await fetch(url, options);
   const mediajson = await mediadata.json();
-  let firstImage = null;
+  let firstImage: any = null;
   for (const item of mediajson) {
     if (item.type === "avatar") {
       firstImage = item;
@@ -41,8 +42,8 @@ export async function teamLogo(team, options = {}) {
   }
 }
 
-export async function teamAwards(team, year, options = {}) {
-  let url = "";
+export async function teamAwards(team: string, year: number, options: any = {}): Promise<any> {
+  let url: string = "";
   if (year) {
     url = `${baseUrl}/team/frc${team}/awards/${year}`;
   } else {
@@ -58,8 +59,8 @@ export async function teamAwards(team, year, options = {}) {
     .then((data) => data);
 }
 
-export async function matchInfo(matchkey, options = {}) {
-  const url = `${baseUrl}/match/${matchkey}`;
+export async function matchInfo(matchkey: string, options: any = {}): Promise<any> {
+  const url: string = `${baseUrl}/match/${matchkey}`;
   options.headers = {
     "X-TBA-Auth-Key": token,
   };
@@ -69,15 +70,15 @@ export async function matchInfo(matchkey, options = {}) {
     .then((data) => data);
 }
 
-export async function teamRobotImage(team, year, options = {}) {
-  const url = `${baseUrl}/team/frc${team}/media/${year}`;
+export async function teamRobotImage(team: string, year: number, options: any = {}): Promise<any> {
+  const url: string = `${baseUrl}/team/frc${team}/media/${year}`;
   options.headers = {
     "X-TBA-Auth-Key": token,
   };
 
   const mediadata = await fetch(url, options);
   const mediajson = await mediadata.json();
-  let firstImage = null;
+  let firstImage: string | null = null;
   for (const item of mediajson) {
     if (item.type === "image" || item.type === "imgur") {
       firstImage = `https://i.imgur.com/${item.foreign_key}.png`;
